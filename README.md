2b9371ec3d9d992756cbca59612ecbcad3c7d3f9
Elmar Athmer <elmar@athmer.org>
2025-04-20 22:12:06 +0200

    setup listenfd support

--- README.md
+++ README.md
@@ -6,10 +6,21 @@ Skeleton for a rust business application
 
 - [nextest](https://nexte.st/)
 - [bacon](https://dystroy.org/bacon/)
+- [systemfd](https://github.com/mitsuhiko/systemfd)
+
+## Feature Flags
+
+Some functions are behind feature flags. Of course, code guarded by features may be deleted
+completely if not needed.
+
+- [`listenfd`](https://github.com/mitsuhiko/listenfd?tab=readme-ov-file#listenfd) supports using
+  externally managed file descriptors.
+
+  This is useful during development (auto-reload) or if using systemd-socket activation.
 
 ## Development
 
-- `bacon run`
+- `bacon run` or `systemfd --no-pid -s http::8080 -- bacon run`
 - `bacon test` or `bacon nextest`
 
 ![Iconoclast](./doc/iconoclast.png)
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use axum::Router;
 #[cfg(not(target_env = "msvc"))]
 use tikv_jemallocator::Jemalloc;
+use tokio::net::TcpListener;
 
 mod handler;
 
@@ -9,11 +10,26 @@ mod handler;
 static GLOBAL: Jemalloc = Jemalloc;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
     let app = Router::new().nest("/hello", handler::hello::init());
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    #[cfg(feature = "listenfd")]
+    let listener = {
+        let mut listenfd = listenfd::ListenFd::from_env();
+        match listenfd.take_tcp_listener(0).unwrap() {
+            Some(listener) => {
+                listener.set_nonblocking(true)?;
+                TcpListener::from_std(listener)
+            }
+            None => TcpListener::bind("0.0.0.0:8080").await,
+        }?
+    };
+    #[cfg(not(feature = "listenfd"))]
+    let listener = TcpListener::bind("0.0.0.0:8080").await?;
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
