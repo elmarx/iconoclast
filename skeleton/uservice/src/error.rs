43241816084bc7d9e28a78cc6ab90241921d90c4
Elmar Athmer <elmar@athmer.org>
2025-05-04 11:00:00 +0200

    wip pretty-error-debug

--- skeleton/uservice/src/error.rs
+++ skeleton/uservice/src/error.rs
@@ -1,8 +1,9 @@
 use config::ConfigError;
 use iconoclast::kafka::KafkaError;
+use thiserror::Error;
 
 /// Container for all errors in different layers/parts of the service
-#[derive(Debug, thiserror::Error)]
+#[derive(Error, pretty_error_debug::Debug)]
 pub enum AppError {
     #[error(transparent)]
     Dal(#[from] repository::Error),
