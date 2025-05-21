fd51e57b4a63218200821df7bd4cdf31c2c74533
Elmar Athmer <elmar@athmer.org>
2025-05-21 22:57:21 +0200

    setup service with outbound repository

--- examples/hexagonal/application/src/outbound.rs
+++ examples/hexagonal/application/src/outbound.rs
@@ -1,1 +1,12 @@
+use domain::{Task, TaskId};
+use errors::RepositoryError;
+use futures::Stream;
 
+#[async_trait::async_trait]
+pub trait TaskRepository: Send + Sync {
+    async fn insert(&self, desc: &str) -> Result<TaskId, RepositoryError>;
+
+    async fn find_by_id(&self, id: TaskId) -> Result<Option<Task>, RepositoryError>;
+
+    fn find_all(&self) -> impl Stream<Item = Result<Task, RepositoryError>>;
+}
--- examples/hexagonal/application/src/service.rs
+++ examples/hexagonal/application/src/service.rs
@@ -1,1 +1,11 @@
+use crate::outbound::TaskRepository;
 
+pub struct TodoService<T: TaskRepository> {
+    repository: T,
+}
+
+impl<T: TaskRepository> TodoService<T> {
+    pub const fn new(repository: T) -> Self {
+        Self { repository }
+    }
+}
