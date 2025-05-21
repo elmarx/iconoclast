1b6c50304d519c622a84a47c6c6fa0155f06a7be
Elmar Athmer <elmar@athmer.org>
2025-05-21 22:57:21 +0200

    setup service with inbound repository

--- examples/hexagonal/application/src/outbound.rs
+++ examples/hexagonal/application/src/outbound.rs
@@ -1,1 +1,12 @@
 
+use domain::Task;
+use errors::SqlxError;
+use futures::Stream;
+
+pub trait TaskRepository {
+    fn insert(&self, desc: &str) -> impl Future<Output = Result<i64, SqlxError>>;
+
+    fn find_by_id(&self, id: i64) -> impl Future<Output = Result<Option<Task>, SqlxError>>;
+
+    fn find_all(&self) -> impl Stream<Item = Result<Task, SqlxError>>;
+}
--- examples/hexagonal/application/src/service.rs
+++ examples/hexagonal/application/src/service.rs
@@ -1,1 +1,12 @@
 
+use crate::outbound::TaskRepository;
+
+struct TodoService<T: TaskRepository> {
+    repository: T,
+}
+
+impl<T: TaskRepository> TodoService<T> {
+    pub const fn new(repository: T) -> Self {
+        Self { repository }
+    }
+}
