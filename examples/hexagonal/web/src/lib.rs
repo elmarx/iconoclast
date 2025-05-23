cc5ebc403db10f6c0a83fee3e45ccd60bc5b4e87
Elmar Athmer <elmar@athmer.org>
2025-05-23 09:36:21 +0200

    add web adapter

--- examples/hexagonal/web/src/lib.rs
+++ examples/hexagonal/web/src/lib.rs
@@ -1,1 +1,14 @@
+use application::inbound;
+pub use axum::Router;
+use axum::response::IntoResponse;
+use std::sync::Arc;
 
+pub fn init(endpoint: impl inbound::Endpoint + 'static) -> Router {
+    Router::new()
+        .route("/", axum::routing::get(index))
+        .with_state(Arc::new(endpoint))
+}
+
+async fn index() -> impl IntoResponse {
+    "Index"
+}
