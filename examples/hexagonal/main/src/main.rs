ae16236ef646d088ce3d270f0a0c276ea2cd47b7
Elmar Athmer <elmar@athmer.org>
2025-05-25 11:41:50 +0200

    implement main/bin for hexagonal demo

--- examples/hexagonal/main/src/main.rs
+++ examples/hexagonal/main/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use crate::wire::wire;
+use iconoclast::{logging, server};
+use tikv_jemallocator::Jemalloc;
+use tracing::info;
+
+mod wire;
+
+// Jemalloc reduces heap-fragmentation and yields a way better memory-profile for the application
+// in almost all cases Jemalloc is the better choice.
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
+const DEFAULT_CONFIG: &str = include_str!("../../config.default.toml");
+
+pub type ServiceSettings = iconoclast::DefaultServiceConfig;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let settings = ServiceSettings::emerge(DEFAULT_CONFIG)?;
+
+    logging::init(&settings.iconoclast.logging).await;
+
+    info!("{settings:?}");
+
+    let router = wire(&settings).await?;
+
+    // TODO: also start the management port. But this would require proper (kafka-) types to use StartupError.
+    server::start(settings.iconoclast.port, router).await?;
+
+    Ok(())
 }
---
+++ examples/hexagonal/main/src/wire.rs
@@ -0,0 +1,15 @@
+use crate::ServiceSettings;
+use adapter_repository as repository;
+use adapter_web as web;
+use application::service::TodoService;
+use std::error::Error;
+
+pub async fn wire(settings: &ServiceSettings) -> Result<web::Router, Box<dyn Error>> {
+    let task_repository = repository::init(settings.database_url.as_deref()).await?;
+
+    let service = TodoService::new(task_repository);
+
+    let router = web::init(service);
+
+    Ok(router)
+}
