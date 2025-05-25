bcaba3fea14e509f96b05bf0c1f8a7a2c11e03d3
Elmar Athmer <elmar@athmer.org>
2025-05-25 11:41:50 +0200

    implement init for repository adapter

--- examples/hexagonal/repository/src/lib.rs
+++ examples/hexagonal/repository/src/lib.rs
@@ -1,2 +1,27 @@
+use sqlx::PgPool;
+use sqlx::postgres::{PgConnectOptions, PgPoolOptions};
+
+pub use task::Repository as TaskRepository;
+
 pub mod task;
 
+/// Initializes the database connection and returns the repositories.
+///
+/// If no database URL is given, the connection parameters are read from the environment.
+/// See [SQLx documentation](https://docs.rs/sqlx/latest/sqlx/postgres/struct.PgConnectOptions.html#parameters).
+///
+/// # Errors
+///
+/// fails if no DB connection could be established
+pub async fn init(url: Option<&str>) -> Result<TaskRepository, sqlx::Error> {
+    let _pool = if let Some(url) = url {
+        PgPoolOptions::new().max_connections(5).connect(url).await?
+    } else {
+        // if no url is given, read connection parameters from env
+        PgPool::connect_with(PgConnectOptions::new()).await?
+    };
+
+    let repo = TaskRepository {};
+
+    Ok(repo)
+}
