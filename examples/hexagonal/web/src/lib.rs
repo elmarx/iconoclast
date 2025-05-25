eecde1de8ac64bec6e6e9abc72f8c25242a19b65
Elmar Athmer <elmar@athmer.org>
2025-05-26 00:24:27 +0200

    implement tests for the web-adapter

--- examples/hexagonal/web/src/lib.rs
+++ examples/hexagonal/web/src/lib.rs
@@ -33,3 +33,61 @@ where
         "<body><h1>Hello, World</h1>{number}<ul>{todos}</ul></body>"
     ))
 }
+
+#[cfg(test)]
+mod test {
+    use application::inbound;
+    use axum::body::Body;
+    use axum::http::Request;
+    use errors::RepositoryError;
+    use futures::stream;
+    use futures::stream::Stream;
+    use http_body_util::BodyExt;
+    use tower::ServiceExt;
+
+    #[derive(Clone)]
+    struct MockEndpoint {
+        todos: Vec<domain::Task>,
+    }
+
+    #[async_trait::async_trait]
+    impl inbound::Endpoint for MockEndpoint {
+        fn list_todos(&self) -> impl Stream<Item = Result<domain::Task, RepositoryError>> + Send {
+            let todos = self.todos.iter().map(|t| Ok(t.clone()));
+            stream::iter(todos)
+        }
+
+        async fn add_todo(&self, _desc: &str) -> Result<domain::TaskId, RepositoryError> {
+            unimplemented!()
+        }
+    }
+
+    #[tokio::test]
+    async fn test_list() {
+        let sample_a = domain::Task {
+            id: domain::TaskId::default(),
+            description: "Todo A".to_string(),
+        };
+        let sample_b = domain::Task {
+            id: domain::TaskId::default(),
+            description: "Todo B".to_string(),
+        };
+
+        let endpoint = MockEndpoint {
+            todos: vec![sample_a.clone(), sample_b.clone()],
+        };
+
+        let router = super::init(endpoint);
+
+        let response = router
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        let body = String::from_utf8(body.to_vec()).unwrap();
+
+        assert!(body.contains("Todo A"));
+        assert!(body.contains("Todo B"));
+    }
+}
