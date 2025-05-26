b314ea2bc4b683586db34b43aedc4a4d692ebb8e
Elmar Athmer <elmar@athmer.org>
2025-05-26 11:52:15 +0200

    define inbound port to handle events

--- examples/hexagonal/application/src/inbound.rs
+++ examples/hexagonal/application/src/inbound.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use domain::TaskId;
+use domain::{TaskId, event};
 use errors::RepositoryError;
 use futures::Stream;
 
@@ -8,4 +8,10 @@ pub trait Endpoint: Send + Sync {
     fn list_todos(&self) -> impl Stream<Item = Result<domain::Task, RepositoryError>> + Send;
 
     async fn add_todo(&self, desc: &str) -> Result<TaskId, RepositoryError>;
+}
+
+pub type EventHandlerError = RepositoryError;
+#[async_trait]
+pub trait TaskEventHandler: Send + Sync {
+    async fn task(&self, e: event::Task) -> Result<(), EventHandlerError>;
 }
