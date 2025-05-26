fe84109dca81fed921c2c5c251457121b701a10d
Elmar Athmer <elmar@athmer.org>
2025-05-26 21:20:03 +0200

    add repository methods

--- examples/hexagonal/application/src/outbound.rs
+++ examples/hexagonal/application/src/outbound.rs
@@ -7,7 +7,11 @@ use futures::Stream;
 pub trait TaskRepository: Send + Sync {
     async fn insert(&self, desc: &str) -> Result<TaskId, RepositoryError>;
 
+    async fn insert_with_id(&self, task_id: TaskId, desc: &str) -> Result<(), RepositoryError>;
+
     async fn find_by_id(&self, id: TaskId) -> Result<Option<Task>, RepositoryError>;
+
+    async fn delete_by_id(&self, id: TaskId) -> Result<bool, RepositoryError>;
 
     fn find_all(&self) -> impl Stream<Item = Result<Task, RepositoryError>> + Send;
 }
--- examples/hexagonal/repository/src/task.rs
+++ examples/hexagonal/repository/src/task.rs
@@ -30,6 +30,18 @@ impl TaskRepository for Repository {
         Ok(TaskId::from(rec.task_id))
     }
 
+    async fn insert_with_id(&self, task_id: TaskId, desc: &str) -> Result<(), RepositoryError> {
+        sqlx::query!(
+            r#"INSERT INTO task (task_id, description) VALUES ($1, $2)"#,
+            task_id.0,
+            desc
+        )
+        .execute(&self.pool)
+        .await?;
+
+        Ok(())
+    }
+
     async fn find_by_id(&self, id: TaskId) -> Result<Option<domain::Task>, RepositoryError> {
         sqlx::query_as!(
             domain::Task,
@@ -38,6 +50,14 @@ impl TaskRepository for Repository {
         )
         .fetch_optional(&self.pool)
         .await
+    }
+
+    async fn delete_by_id(&self, id: TaskId) -> Result<bool, RepositoryError> {
+        let affected = sqlx::query!("DELETE FROM task WHERE task_id = $1", id.0)
+            .execute(&self.pool)
+            .await?;
+
+        Ok(0 < affected.rows_affected())
     }
 
     fn find_all(&self) -> impl Stream<Item = Result<domain::Task, RepositoryError>> {
@@ -50,6 +70,7 @@ impl TaskRepository for Repository {
 mod test {
     use super::Repository;
     use application::outbound::TaskRepository;
+    use domain::TaskId;
     use futures::TryStreamExt;
     use sqlx::PgPool;
 
@@ -69,6 +90,25 @@ mod test {
     }
 
     #[sqlx::test]
+    async fn test_insert_with_id(pool: PgPool) {
+        let repo = Repository::new(pool);
+
+        let id = TaskId::new();
+
+        repo.insert_with_id(id, "test with a given id")
+            .await
+            .unwrap();
+
+        let actual = repo.find_by_id(id).await.unwrap();
+        let expected = domain::Task {
+            id,
+            description: "test with a given id".to_string(),
+        };
+
+        assert_eq!(actual, Some(expected));
+    }
+
+    #[sqlx::test]
     async fn test_find_all(pool: PgPool) {
         let repo = Repository::new(pool);
 
@@ -80,5 +120,18 @@ mod test {
         assert_eq!(actual.len(), 2);
         assert_eq!(actual[0].description, "todo 1");
         assert_eq!(actual[1].description, "todo 2");
+    }
+
+    #[sqlx::test]
+    async fn test_delete_by_id(pool: PgPool) {
+        let repo = Repository::new(pool);
+
+        let sample_id = repo.insert("test delete").await.unwrap();
+
+        let is_deleted = repo.delete_by_id(sample_id).await.unwrap();
+
+        assert!(is_deleted);
+        let actual = repo.find_by_id(sample_id).await.unwrap();
+        assert_eq!(actual, None);
     }
 }
