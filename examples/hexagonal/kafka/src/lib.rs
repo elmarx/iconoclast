e40e3b4647d768056f3edcabb1c11f6a3ae8f8a9
Elmar Athmer <elmar@athmer.org>
2025-05-26 11:52:15 +0200

    implement kafka message handler calling the event-handler port

--- examples/hexagonal/kafka/src/lib.rs
+++ examples/hexagonal/kafka/src/lib.rs
@@ -1,4 +1,16 @@
+use application::inbound;
+
+mod from;
+mod message_handler;
 mod topic;
 
 pub use topic::DecodeError;
 pub use topic::TOPICS;
+
+// some helper structs until we can do `trait MyMessageHandler = MessageHandler<inbound::EventHandlerError, Message = topic::TopicMessages>`…
+/// the [`MessageHandler`]'s application-error (type `AE`)
+pub type ApplicationError = inbound::EventHandlerError;
+/// the Message-type of the [`MessageHandler`]
+pub type Message = topic::TopicMessages;
+
+pub use message_handler::KafkaListener;
---
+++ examples/hexagonal/kafka/src/message_handler.rs
@@ -0,0 +1,29 @@
+//! implementation for the [`iconoclast::kafka::MessageHandler`]
+
+use crate::{ApplicationError, topic};
+use application::inbound;
+use futures::TryFutureExt;
+use iconoclast::kafka::MessageHandler;
+
+pub struct KafkaListener<T: inbound::TaskEventHandler> {
+    todo_event_handler: T,
+}
+
+impl<T: inbound::TaskEventHandler> KafkaListener<T> {
+    pub const fn new(todo_event_handler: T) -> Self {
+        Self { todo_event_handler }
+    }
+}
+
+impl<T: inbound::TaskEventHandler> MessageHandler<ApplicationError> for KafkaListener<T> {
+    type Message = topic::TopicMessages;
+
+    fn handle(
+        &self,
+        kafka_message: Self::Message,
+    ) -> impl Future<Output = Result<(), ApplicationError>> {
+        match kafka_message {
+            Self::Message::Todo(t) => self.todo_event_handler.task(t.into()).map_ok(|_| ()),
+        }
+    }
+}
