3a039a0deb9969dd38f556824384944ee5d18e5f
Elmar Athmer <elmar@athmer.org>
2025-06-02 20:17:00 +0200

    remove the old (layered) skeleton

--- examples/skeleton/logic/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! Implementation the business logic.
-pub use repository::Error as RepositoryError;
-pub use repository::SqlxError;
-
-#[derive(thiserror::Error, Debug)]
-pub enum Error {}
--- examples/skeleton/uservice/src/settings.rs
+++
@@ -1,8 +0,0 @@
-const DEFAULT_CONFIG: &str = include_str!("../../config.default.toml");
-
-// if this service needs more settings, replace the Settings with a service-specific Settings/Config-Struct
-pub type Settings = iconoclast::DefaultServiceConfig;
-
-pub fn emerge() -> Result<Settings, iconoclast::config::ConfigError> {
-    Settings::emerge(DEFAULT_CONFIG)
-}
