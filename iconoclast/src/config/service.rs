fae389a3523fa4c945dad7df5430da5622f33682
Elmar Athmer <elmar@athmer.org>
2025-06-04 21:18:16 +0200

    feat: make database_url unsettable with an empty string

--- iconoclast/src/config/service.rs
+++ iconoclast/src/config/service.rs
@@ -1,13 +1,16 @@
 use crate::config::builder::ServiceConfig;
 use crate::config::iconoclast;
 use serde::Deserialize;
+use serde_with::{NoneAsEmptyString, serde_as};
 
+#[serde_as]
 #[derive(Debug, Deserialize)]
 pub struct DefaultServiceConfig {
     /// generic configuration for all iconoclast-like services
     pub iconoclast: iconoclast::Config,
 
-    // if no url is given, connection parameters will be read from env: https://docs.rs/sqlx/latest/sqlx/postgres/struct.PgConnectOptions.html#parameters
+    // if no url (or an empty string) is given, connection parameters will be read from env: https://docs.rs/sqlx/latest/sqlx/postgres/struct.PgConnectOptions.html#parameters
+    #[serde_as(as = "NoneAsEmptyString")]
     pub database_url: Option<String>,
 
     /// kafka configuration
